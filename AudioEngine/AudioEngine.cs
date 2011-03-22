d37dc52127a75c1313469c29da77ed9d9a9ffd46
Bogdan <bogdan-alexandru.panait@some.ox.ac.uk>
2011-03-22 15:14:46 +0000

    Added stub classes for AudioEngine and AudioSummary
    Methods do nothing for now, just interface.

---
+++ AudioEngine/AudioEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SurfaceTower.AudioEngine
+{
+    class AudioEngine
+    {
+        private BaseModel baseModel;
+
+        public AudioEngine(BaseModel baseModel)
+        {
+            this.baseModel = baseModel;
+        }
+
+        public AudioSummary Tick(int barIndicator)
+        {
+            return null;
+        }
+    }
+}
