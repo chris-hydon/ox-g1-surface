d9d6f0f80235006a04969c73bb2c3c689f216bbc
Bogdan <bogdan-alexandru.panait@some.ox.ac.uk>
2011-05-04 22:14:43 +0100

    Adds an AudioResources project to host the audio files needed.

---
+++ AudioResources/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AudioResources")]
+[assembly: AssemblyProduct("AudioResources")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a03966c7-3e9a-460a-a8e0-74c9259fca58")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ AudioResources/README.txt
@@ -0,0 +1,9 @@
+﻿This extra project is simply here to store the XACT and audio files needed to play the audio of the game.
+We decided to store this in a separate project as VS tries to rebuild these files on every compile and that
+takes up to a minute, which is way too long.
+
+I don't know if build settings get transfered over git so, if your compile is taking too long, make sure
+you go to Build > Configuration Manager and untick AudioResources in the Build column.
+
+Also note: you'll need to build the AudioResources project at least once, otherwise you'll get runtime
+errors because of missing files.
