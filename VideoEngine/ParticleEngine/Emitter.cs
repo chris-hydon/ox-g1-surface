6f6cb798aa06f0e22b4646a3538a5e0bbcf29e61
Dan Nichol <daniel.nichol@st-annes.ox.ac.uk>
2011-05-06 18:57:47 +0100

    Added a simple particle engine, improved implementation to follow

---
+++ VideoEngine/ParticleEngine/Emitter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SurfaceTower.VideoEngine.ParticleEngine
+{ 
+    public class Emitter
+    {
+        const int MAXPARTICLES = 200;
+        private int numberOfParticles;
+        private Vector2 position;
+        private ICollection<Particle> particles;
+        private Texture2D sprite;
+        private Random random;
+
+        public Emitter(Vector2 position, Texture2D sprite)
+        {
+            this.position = position;
+            this.sprite = sprite;
+            particles = new List<Particle>(MAXPARTICLES);
+            numberOfParticles = 0;
+            this.random = new Random();
+        }
+
+        public void Update(){
+            Particle p;
+            for (int i = numberOfParticles; i < MAXPARTICLES; i++)
+            {
+                particles.Add(newParticle());
+                numberOfParticles++;
+            }
+            for (int i = 0; i < numberOfParticles; i++)
+            {
+                p = particles.ElementAt<Particle>(i);
+                if (p.timeToLive <= 0){
+                    particles.Remove(p);
+                    numberOfParticles--;
+                }
+            }
+            for (int i = 0; i < numberOfParticles; i++)
+            {
+                particles.ElementAt<Particle>(i).Update();
+            }
+        }
+
+        private Particle newParticle()
+        {
+            Vector2 v = (float)random.NextDouble()*0.05f*(new Vector2(random.Next(100), random.Next(100)));
+            if (random.Next(3) > 1)
+            {
+                v.X *= -1;
+            }
+            if (random.Next(3) > 1)
+            {
+                v.Y *= -1;
+            }
+            Vector2 p = position;
+            float a = (float)random.NextDouble();
+            float av = (float)random.NextDouble();
+            int s = random.Next(5);
+            int ttl = random.Next(150)+1;
+            Particle particle = new Particle(v, p, a, av, s, ttl, sprite);
+            return particle;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (Particle p in particles)
+            {
+                p.Draw(spriteBatch);
+            }
+        }
+
+    }
+}
---
+++ VideoEngine/ParticleEngine/PEngine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SurfaceTower.VideoEngine.ParticleEngine
+{
+    public class PEngine
+    {
+        const int MAXEMITTERS = 50;
+        private ICollection<Emitter> emitters;
+        private SimpleView view;
+        private Texture2D tex;
+
+        public PEngine(SimpleView view)
+        {
+            this.view = view;
+            emitters = new List<Emitter>(MAXEMITTERS);
+            tex = view.content.Load<Texture2D>("particle");
+        }
+
+        public void addEmitter(Vector2 position)
+        {
+            emitters.Add(new Emitter(position, tex));
+        }
+
+        public void Update()
+        {
+            foreach (Emitter e in emitters)
+            {
+                e.Update();
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Emitter e in emitters)
+            {
+                e.Draw(view.spritebatch);
+            }
+
+        }
+
+
+
+
+
+    }
+}
